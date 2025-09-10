0797b2fff03b014aeef970f6351fb54559ea98fe
Divyanshu416Rai <divyanshukumarrai832@gmail.com>
2025-09-10 09:08:26 +0530

    FSDpractice

---
+++ Day 4/index.js
@@ -0,0 +1,11 @@
+let firstname = "Divyanshu";
+let lastname = "Rai";
+let message = "Namaste";
+let outputmessage = message + ", " + firstname + " " + lastname;
+//console.log(outputmessage);
+
+let sum = 2-2 + "22";
+// console.log(sum);
+// console.log(typeof(sum));
+
+let strToNum
